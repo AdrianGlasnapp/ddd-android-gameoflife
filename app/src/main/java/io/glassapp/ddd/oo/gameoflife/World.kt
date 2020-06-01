4c10a240cff4b60dc1e0f0c82369c5b8df9429a6
AdrianGlasnapp <adrian.glasnapp@gmail.com>
2020-06-01 07:45:03 +0200

    World next generation method added

--- app/src/main/java/io/glassapp/ddd/oo/gameoflife/World.kt
+++ app/src/main/java/io/glassapp/ddd/oo/gameoflife/World.kt
@@ -6,11 +6,83 @@ class World(
     val id: UUID,
     val rows: Int,
     val columns: Int,
-    val cells: Set<Cell>
+    var cells: Set<Cell>
 ) {
+    private val liveNeighborsForSurvival: Set<Int> = setOf(2, 3)
+    private val liveNeighborsForBirth: Set<Int> = setOf(3)
 
     fun nextGeneration() {
+        cells = survivingCells() + bornCells()
+    }
 
+    private fun survivingCells(): Set<Cell> {
+        return cells
+            .filter { survives(it.position) }
+            .toSet()
+    }
+
+
+    private fun bornCells(): Set<Cell> {
+        return deadNeighbors(cells)
+            .filter { born(it) }
+            .map { Cell(it) }
+            .toSet()
+    }
+
+    private fun deadNeighbors(cells: Set<Cell>): Set<Position> {
+        return cells
+            .flatMap { deadNeighbors(it.position) }
+            .distinct()
+            .toSet()
+    }
+
+    private fun deadNeighbors(cell: Position): List<Position> {
+        return neighbors(cell)
+            .filterNot { isAlive(it) }
+    }
+
+    private fun liveNeighbors(cell: Position): Set<Position> {
+        return neighbors(cell)
+            .filter { isAlive(it) }
+            .toSet()
+    }
+
+    private fun isAlive(position: Position): Boolean {
+        return cells.contains(Cell(position))
+    }
+
+    private fun neighbors(position: Position): Set<Position> {
+        val cell = Cell(position)
+        return setOf(
+            Position(cell.position.row - 1, cell.position.column),
+            Position(cell.position.row - 1, cell.position.column - 1),
+            Position(cell.position.row, cell.position.column - 1),
+            Position(cell.position.row + 1, cell.position.column - 1),
+            Position(cell.position.row + 1, cell.position.column),
+            Position(cell.position.row + 1, cell.position.column + 1),
+            Position(cell.position.row, cell.position.column + 1),
+            Position(cell.position.row - 1, cell.position.column + 1)
+        )
+    }
+
+    private fun survives(cell: Position): Boolean {
+        return survives(countLiveNeighbors(cell))
+    }
+
+    private fun born(cell: Position): Boolean {
+        return born(countLiveNeighbors(cell))
+    }
+
+    private fun survives(liveNeighbors: Int): Boolean {
+        return liveNeighborsForSurvival.contains(liveNeighbors)
+    }
+
+    private fun born(liveNeighbors: Int): Boolean {
+        return liveNeighborsForBirth.contains(liveNeighbors)
+    }
+
+    private fun countLiveNeighbors(cell: Position): Int {
+        return liveNeighbors(cell).count()
     }
 
     companion object {
@@ -19,7 +91,7 @@ class World(
             val cells = mutableSetOf<Cell>()
             for (i in 0 until rows) {
                 for (j in 0 until columns) {
-                    if ((Math.random() < 0.3)) {
+                    if (Math.random() < 0.3) {
                         cells.add(Cell(Position(i, j)))
                     }
                 }
@@ -40,7 +112,5 @@ class World(
                 cells
             )
         }
-
-
     }
 }
--- app/src/test/java/io/glassapp/ddd/oo/gameoflife/WorldTest.kt
+++ app/src/test/java/io/glassapp/ddd/oo/gameoflife/WorldTest.kt
@@ -3,17 +3,30 @@ package io.glassapp.ddd.oo.gameoflife
 import org.junit.Test
 
 import org.junit.Assert.*
+import java.util.*
 
 class WorldTest {
 
     @Test
     fun `newWorld creates new World with random live Cells`() {
-        val rows = 24
-        val columns = 16
+        val rows = 9
+        val columns = 9
 
         val world = World.newWorld(rows, columns)
 
         assertEquals(rows, world.rows)
         assertEquals(columns, world.columns)
+    }
+
+    @Test
+    fun `nextGeneration is empty when Cells have no neighbors`() {
+        val rows = 9
+        val columns = 9
+        val cells = setOf(Cell(Position(0, 1)), Cell(Position(3, 3)))
+        val world = World.newWorld(UUID.randomUUID(), rows, columns, cells)
+
+        world.nextGeneration()
+
+        assertEquals(0, world.cells.size)
     }
 }
