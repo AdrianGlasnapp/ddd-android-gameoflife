c623f77140f6c7e55bab8496c3d527da0d0a8b01
AdrianGlasnapp <adrian.glasnapp@gmail.com>
2020-06-05 15:50:57 +0200

    More tests added

--- app/src/test/java/io/glassapp/ddd/oo/gameoflife/WorldTest.kt
+++ app/src/test/java/io/glassapp/ddd/oo/gameoflife/WorldTest.kt
@@ -19,14 +19,49 @@ class WorldTest {
     }
 
     @Test
-    fun `nextGeneration is empty when Cells have no neighbors`() {
+    fun `nextGeneration is empty when Cell have no neighbors`() {
         val rows = 9
         val columns = 9
-        val cells = setOf(Cell(Position(0, 1)), Cell(Position(3, 3)))
+        val cells = setOf(
+            Cell(Position(0, 1)),
+            Cell(Position(3, 3))
+        )
         val world = World.newWorld(UUID.randomUUID(), rows, columns, cells)
 
         world.nextGeneration()
 
         assertEquals(0, world.cells.size)
+    }
+
+    @Test
+    fun `nextGeneration is empty when Cell have 1 neighbor`() {
+        val rows = 9
+        val columns = 9
+        val cells = setOf(
+            Cell(Position(0, 1)),
+            Cell(Position(3, 3)),
+            Cell(Position(1, 1))
+        )
+        val world = World.newWorld(UUID.randomUUID(), rows, columns, cells)
+
+        world.nextGeneration()
+
+        assertEquals(0, world.cells.size)
+    }
+
+    @Test
+    fun `nextGeneration Cell survives when have 2 neighbor`() {
+        val rows = 9
+        val columns = 9
+        val cells = setOf(
+            Cell(Position(0, 0)),
+            Cell(Position(0, 1)),
+            Cell(Position(0, 2))
+        )
+        val world = World.newWorld(UUID.randomUUID(), rows, columns, cells)
+
+        world.nextGeneration()
+
+        assertEquals(3, world.cells.size)
     }
 }
