c1b5ada4c08e6317397d1f0a79781c28a8abc2bf
AdrianGlasnapp <adrian.glasnapp@gmail.com>
2020-07-16 07:03:51 +0200

    ViewModel refactor

--- example-ddd-oo/gol-ddd-oo-app/src/main/java/io/glassapp/example/ddd/oo/ui/game/GameFragment.kt
+++ example-ddd-oo/gol-ddd-oo-app/src/main/java/io/glassapp/example/ddd/oo/ui/game/GameFragment.kt
@@ -5,9 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import io.glassapp.ddd.oo.gameoflife.GolContext
-import io.glassapp.ddd.oo.gameoflife.domain.WorldData
 import io.glassapp.example.ddd.oo.R
 import kotlinx.android.synthetic.main.game_fragment.*
 
@@ -18,8 +17,6 @@ class GameFragment : Fragment() {
     }
 
     private lateinit var viewModel: GameViewModel
-    private val gofContext = GolContext()
-    private lateinit var worldData: WorldData
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,31 +28,16 @@ class GameFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        viewModel = ViewModelProviders.of(this).get(GameViewModel::class.java)
-
-        // TODO: move to ViewModel
         val rows = 24
         val columns = 16
-        val iterations = 100
-        val sleep = 500L
-
         val pixelGrid = PixelGridView(requireContext(), columns, rows)
         boardContainer.addView(pixelGrid)
 
-        worldData = gofContext.gameService.createNewWorld(rows, columns)
-
-        Thread(
-            Runnable {
-                for (i in 0 until iterations) {
-                    worldData = gofContext.gameService.generateNextIteration(worldData.id)
-
-                    activity?.runOnUiThread {
-                        pixelGrid.render(worldData)
-                    }
-                    Thread.sleep(sleep)
-                }
-            }
-        ).start()
+        viewModel = ViewModelProviders.of(this).get(GameViewModel::class.java)
+        viewModel.worldData().observe(this.viewLifecycleOwner, Observer {
+            pixelGrid.render(it)
+        })
+        viewModel.start(rows, columns)
     }
 
 }
--- example-ddd-oo/gol-ddd-oo-app/src/main/java/io/glassapp/example/ddd/oo/ui/game/GameViewModel.kt
+++ example-ddd-oo/gol-ddd-oo-app/src/main/java/io/glassapp/example/ddd/oo/ui/game/GameViewModel.kt
@@ -1,7 +1,40 @@
 package io.glassapp.example.ddd.oo.ui.game
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import io.glassapp.ddd.oo.gameoflife.GolContext
+import io.glassapp.ddd.oo.gameoflife.domain.WorldData
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
 class GameViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val gofContext = GolContext()
+    private val compositeDisposable = CompositeDisposable()
+    private val worldData: MutableLiveData<WorldData> = MutableLiveData()
+
+    fun start(rows: Int, columns: Int) {
+        val newWorldData = gofContext.gameService.createNewWorld(rows, columns)
+        worldData.value = newWorldData
+
+        Observable.interval(500, TimeUnit.MILLISECONDS)
+            .map { gofContext.gameService.generateNextIteration(newWorldData.id) }
+            .subscribeOn(Schedulers.computation())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe { worldData.value = it }
+            .let { compositeDisposable.add(it) }
+    }
+
+    fun worldData(): LiveData<WorldData> {
+        return worldData
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.clear()
+    }
+
 }
